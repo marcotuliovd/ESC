502f06763a84ecffaf61d80fc0e01e9ff06a543d
Marco Túlio <marcotuliodiniz2257@gmail.com>
2023-09-28 20:18:32 -0300

    ✨ create migrations

--- Back/Platform/migrations/0001_create_table.sql
+++ Back/Platform/migrations/0001_create_table.sql
@@ -3,13 +3,27 @@ CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
 SET TIMEZONE="America/Sao_Paulo";
 
 -- Create  table
-CREATE TABLE public.vagas (
-    id SERIAL PRIMARY KEY,
-    porte VARCHAR(10) NOT null,
-    ocupacao TIMESTAMP DEFAULT NOW(),
-    dono VARCHAR(255),
-    telefone VARCHAR(255),
-    tipo_de_veiculo VARCHAR(255),
-    placa VARCHAR(10),
-    modelo VARCHAR(255)
+CREATE TABLE public.clients (
+    id serial PRIMARY KEY,
+    username varchar(255),
+    name varchar(255),
+    phone varchar(15),
+    created_at timestamp
+);
+
+CREATE TABLE public.vehicle (
+    id serial PRIMARY KEY,
+    owner varchar(255),
+    title varchar(255),
+    type varchar(255),
+    placa varchar(10),
+    created_at timestamp
+);
+
+CREATE TABLE public.space (
+    id serial PRIMARY KEY,
+    vehicle_id integer,
+    type varchar(255),
+    created_at timestamp,
+    FOREIGN KEY (vehicle_id) REFERENCES vehicle(id)
 );
